d0be1ecd9c845047d67ed2e2cb0ffe059f024006
niloytalukde <niloytalukderme@gmail.com>
2025-06-29 01:00:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Components/Sidebar';
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar></Sidebar>
+    </div>
+  );
+};
+
+export default App;
