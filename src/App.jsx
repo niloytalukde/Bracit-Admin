ebd47a26cde471026bec99bbdb95334004db7063
niloytalukde <niloytalukderme@gmail.com>
2025-06-29 12:58:55 +0530

    added all route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import Sidebar from "./Components/Sidebar";
 import Topbar from "./Components/Topbar";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
+import Advising from "./Pages/Advising";
+import Schedule from "./Pages/Schedule";
+import ScholarshipHistory from "./Pages/ScholarshipHistory";
+import CourseDrop from "./Pages/CourseDrop";
+import SemesterDrop from "./Pages/SemesterDrop";
+import DepartmentChange from "./Pages/DepartmentChange";
+import Grades from "./Pages/Grades";
+import Probation from "./Pages/Probation";
+import Payslip from "./Pages/Payslip";
 
 const App = () => {
   const [login, setLogin] = useState(false);
@@ -12,6 +21,15 @@ const App = () => {
       {!login ? (
         <Routes>
           <Route path="/" element={<Login setLogin={setLogin} />} />
+          <Route path="/advising" element={<Advising/>} />
+          <Route path="/schedule" element={<Schedule/>} />
+          <Route path="/scholarship-history" element={<ScholarshipHistory/>} />
+          <Route path="/course-drop" element={<CourseDrop/>} />
+          <Route path="/semester-drop" element={<SemesterDrop/>} />
+          <Route path="/department-change" element={<DepartmentChange/>} />
+          <Route path="/grades" element={<Grades/>} />
+          <Route path="/probation" element={<Probation/>} />
+          <Route path="/payslip" element={<Payslip/>} />
         </Routes>
       ) : (
         <div className="grid grid-cols-12 min-h-screen">
---
+++ src/Pages/Grades.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grades = () => {
+    return (
+        <div>
+            <h1>Grade</h1>
+        </div>
+    );
+};
+
+export default Grades;
